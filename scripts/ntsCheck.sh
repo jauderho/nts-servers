81297dcac99a99b2d5ee0a283f21aa5a97aab4d7
Jauder Ho <jauderho@users.noreply.github.com>
2024-09-30 20:34:42 +0000

    Add ntsCheck.sh script
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

---
+++ scripts/ntsCheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if argument is passed
+if [ -z "$1" ]; then
+  echo "Usage: $0 <NTS_SERVER>"
+  exit 1
+fi
+
+# Assign the argument to a variable
+NTS_SERVER=$1
+
+# Run the chronyd command with the argument
+chronyd -Q -t 3 "server $NTS_SERVER iburst nts maxsamples 1"
